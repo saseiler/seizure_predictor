c6cc17bbf902f993865a157bb8200f75110e9ac4
Shreyash Kumar <shrekumar@microsoft.com>
2022-09-23 11:28:38 -0700

    Add data and perform minor changes to loader

--- Utils/data_loader.py
+++ Utils/data_loader.py
@@ -3,7 +3,7 @@ import pandas as pd
 def load_patient_activity_data(patient_id):
     
     df_health_data = pd.read_csv(f"P{patient_id} sim.csv").dropna()
-    df_processed = df_health_data.drop(columns=['seconds_since_hr_recording', 'Unnamed: 0', 'date_time']).reset_index()
+    df_processed = df_health_data.drop(columns=['seconds_since_hr_recording', 'index', 'date_time']).reset_index()
     return df_processed
 
 def load_seizure_data(patient_id):
--- model.py
+++ model.py
@@ -4,7 +4,6 @@ from sklearn.ensemble import RandomForestClassifier
 def oversampler(df_train):
 
     ros = RandomOverSampler(random_state=42)
-    print(df_train.columns)
     # fit predictor and target variable
     x_ros, y_ros = ros.fit_resample(df_train.drop(columns=['index_x', 'index_y']), df_train['index_y'])
     return x_ros, y_ros
